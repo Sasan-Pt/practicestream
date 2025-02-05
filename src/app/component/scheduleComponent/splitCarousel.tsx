37e8c2e26643368fbf33ae0d4616b8c58c02a957
sasan <sasan.pt.tec@gmail.com>
2025-02-05 20:14:03 +0330

    added date

--- src/app/component/scheduleComponent/splitCarousel.tsx
+++ src/app/component/scheduleComponent/splitCarousel.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Carousel,
   CarouselContent,
@@ -5,28 +6,46 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import Kurologo from "../../../../public/Imagebannerlogo.png";
+import dayjs from "dayjs";
+import { useState } from "react";
 
 const SplitCarousel = () => {
+  const [daysInMonth, setDaysInMonth] = useState(() => {
+    const year = dayjs().year();
+    const month = dayjs().month();
+    const daysInMonth = dayjs(`${year}-${month + 1}-01`).daysInMonth();
+    const monthName = dayjs(`${year}-${month + 1}-01`).format("MMMM");
+
+    return Array.from({ length: daysInMonth }, (_, i) => {
+      const date = dayjs(`${year}-${month + 1}-${i + 1}`);
+      return {
+        day: date.format("D"),
+        weekday: date.format("dddd"),
+        month: monthName,
+      };
+    });
+  });
+
   return (
     <div>
       <Carousel>
-        <CarouselContent>
-          <CarouselItem className="basis-1/3">
+        <CarouselContent className="gap-x-4">
+          <CarouselItem className="basis-1/3 bg-white">
+            <Image src={Kurologo} alt="Loading..." />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3 bg-white">
+            <Image src={Kurologo} alt="Loading..." />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3 bg-white">
             <Image src={Kurologo} alt="Loading..." />
           </CarouselItem>
           <CarouselItem className="basis-1/3">
             <Image src={Kurologo} alt="Loading..." />
           </CarouselItem>
-          <CarouselItem className="basis-1/3">
+          <CarouselItem className="basis-1/3 bg-white">
             <Image src={Kurologo} alt="Loading..." />
           </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <Image src={Kurologo} alt="Loading..." />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <Image src={Kurologo} alt="Loading..." />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
+          <CarouselItem className="basis-1/3 bg-white">
             <Image src={Kurologo} alt="Loading..." />
           </CarouselItem>
         </CarouselContent>
