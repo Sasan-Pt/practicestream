421d69b740b1c938f73a441567e7a269007a15fd
sasan <sasan.pt.tec@gmail.com>
2025-02-06 02:10:43 +0330

    added auto scroll

--- src/app/component/scheduleComponent/splitCarousel.tsx
+++ src/app/component/scheduleComponent/splitCarousel.tsx
@@ -3,16 +3,22 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselApi,
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import Kurologo from "../../../../public/Imagebannerlogo.png";
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SplitCarousel = () => {
-  const [daysInMonth, setDaysInMonth] = useState(() => {
+  const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
+  const [currentDate, setCurrentDate] = useState(() => {
     const year = dayjs().year();
     const month = dayjs().month();
+    return { year, month };
+  });
+  const [daysInMonth, setDaysInMonth] = useState(() => {
+    const { year, month } = currentDate;
     const daysInMonth = dayjs(`${year}-${month + 1}-01`).daysInMonth();
     const monthName = dayjs(`${year}-${month + 1}-01`).format("MMMM");
 
@@ -26,9 +32,23 @@ const SplitCarousel = () => {
     });
   });
 
+  useEffect(() => {
+    async function scrollToCurrentDate() {
+      const today = await Number(dayjs().format("D"));
+      await carouselApi?.scrollTo(today);
+    }
+    scrollToCurrentDate();
+  }, [currentDate, carouselApi]);
+
+  const scrollToIndex = (index: number) => {
+    const today = Number(dayjs().format("D"));
+    carouselApi?.scrollTo(today);
+  };
+
   return (
     <div>
-      <Carousel>
+      <button onClick={() => scrollToIndex(2)}>click me</button>
+      <Carousel setApi={setCarouselApi}>
         <CarouselContent className="gap-x-4">
           <CarouselItem className="basis-1/3 bg-white">
             <Image src={Kurologo} alt="Loading..." />
