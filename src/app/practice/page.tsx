eedb24df5190ed56adcd007d11c078f7d3d6cdd0
sasan <sasan.pt.tec@gmail.com>
2025-03-23 02:53:22 +0330

    added practcetype

---
+++ src/app/practice/page.tsx
@@ -0,0 +1,10 @@
+const Practice=()=>{
+    return (
+        <div className="flex gap-x-4 text-white col-start-2">
+            <input></input>
+            <button>delete me</button>
+            <button>add me</button>
+        </div>
+        )
+}
+export default Practice;
