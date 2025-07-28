77bf18fa77758505b8a46bd7fd2f061945cbfda4
Ooze <sasan.pt.tec@gmail.com>
2025-07-28 07:22:07 +0330

    fixed footer

--- src/app/component/footer/footer.tsx
+++ src/app/component/footer/footer.tsx
@@ -3,7 +3,7 @@ import footerImage from "../../../../public/footer-icon.png";
 
 const Footer = () => {
   return (
-    <div className="bg-[#1c1c1c] text-white pb-8 w-full col-span-full grid grid-cols-[4%_92%_4%] md:grid-cols-[5%_90%_5%] relative">
+    <div className="bg-[#1c1c1c] text-white  w-full col-span-full grid grid-cols-[4%_92%_4%] md:grid-cols-[5%_90%_5%] ">
       <div className="col-start-2">
         <div className="flex gap-x-8 justify-between">
           <div>
@@ -12,7 +12,9 @@ const Footer = () => {
             <div>sadasd</div>
             <div>asdasd</div>
           </div>
-          <Image src={footerImage} alt="something" />
+          <div className="relative  -translate-y-12 translate-x-2 w-[260px] h-[320px]">
+            <Image src={footerImage} alt="something" width={260} height={290} />
+          </div>
         </div>
       </div>
     </div>
